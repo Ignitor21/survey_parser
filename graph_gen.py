3a94a677d52a35f6ca90518b5fb74e52871f5fdd
George Samoylov <Ignit218@yandex.ru>
2025-03-03 14:32:16 +0300

    Changed graph_gen args

--- graph_gen.py
+++ graph_gen.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-def generate_graph_numbers(categories : list, values : list, total_amount : int):
+def generate_graph_numbers(statistic : dict, total_amount : int, title : str):
     
     # Создание столбчатой диаграммы
+    categories = list(statistic.keys())
+    values = list(statistic.values())
     plt.figure(figsize=(16, 9)) 
     plt.bar(categories, values, color = 'RoyalBlue', width = 0.5)
     plt.axhline(y = total_amount, color = 'silver', linestyle = '--', label = 'Общее количество опрошенных')
@@ -13,14 +15,15 @@ def generate_graph_numbers(categories : list, values : list, total_amount : int)
         plt.text(i, value/2, str(value), ha = 'center', fontsize = 12, bbox = dict(facecolor = 'white', alpha = .3))
 
     # Настройки графика
-    plt.title('Какими материалами Вы пользовалилсь при изучении курса?', fontsize = 14)
+    plt.title(title, fontsize = 14)
     plt.tick_params(axis = 'both', labelsize = 14)
 
-    plt.savefig('C:/Users/d.r.tikhonov/Documents/Reports/images/materials_matan_1.png', dpi = 300, bbox_inches = 'tight')
+    plt.savefig('/home/jorik/survey/images/001-image.png', dpi = 300, bbox_inches = 'tight')
     plt.show()
 
-# Test
-# categories = ['Смотрел \n лекции', 'Посещал \n лекции', 'Посещал \n семинары', 'Читал \n литературу', 'Читал конспекты \n лекций лектора', 'Другое']
-# values = [38, 50, 65, 43, 43, 5]
-
-# generate_graph_numbers(categories, values, 66)
+#Test
+categories = ['Смотрел \n лекции', 'Посещал \n лекции', 'Посещал \n семинары', 'Читал \n литературу', 'Читал конспекты \n лекций лектора', 'Другое']
+values = [38, 50, 65, 43, 43, 5]
+statistic = dict(zip(categories, values))
+title = "Какими материалами Вы пользовалилсь при изучении курса?"
+generate_graph_numbers(statistic, 66, title)
