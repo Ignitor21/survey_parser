a5933afbfabc5426144b641c13500a847222495f
George Samoylov <Ignit218@yandex.ru>
2025-03-04 11:59:55 +0300

    Modified graph plotting

--- graph_gen.py
+++ graph_gen.py
@@ -4,9 +4,18 @@ import os
 def generate_graph_numbers(statistic : dict, total_amount : int, title : str, filename : str):
     # Создание столбчатой диаграммы
     categories = list(statistic.keys())
+    values = []
     if all(type(item) == str for item in categories):
         categories = [cat.replace(' ', '\n') for cat in categories]
-    values = list(statistic.values())
+        values = list(statistic.values())
+    else:
+        categories = list(range(1, 11))
+        for mark in categories:
+            if mark not in statistic.keys():
+                values.append(0)
+            else:
+                values.append(statistic[mark])
+
     plt.figure(figsize=(16, 9)) 
     bars = plt.bar(categories, values, color = 'RoyalBlue', width = 0.5)
     plt.axhline(y = total_amount, color = 'silver', linestyle = '--', label = 'Общее количество опрошенных')
@@ -20,7 +29,9 @@ def generate_graph_numbers(statistic : dict, total_amount : int, title : str, fi
     # Настройки графика
     plt.title(title, fontsize = 14)
     plt.tick_params(axis = 'both', labelsize = 14)
-
+    if any(type(item) == int for item in categories):
+        plt.xticks(range(0, 11))
+        
     filepath = os.path.join('images', f'{filename}.png')
     plt.savefig(filepath, dpi = 300, bbox_inches='tight')  # bbox_inches='tight' обрезает лишние поля
     #plt.show()
